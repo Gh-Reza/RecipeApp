e8fd9a35ac85016094053061f75c976722a98add
Marc raphael <marcraphael979@gmail.com>
2023-11-14 12:01:02 +0100

    Add devise to user
    - Add user model
    - Add views
    - Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
 
   # Defines the root path route ("/")
+  get '/foods', to: 'foods#index', as: 'user_root'
   resources :foods, only: [:index]
   resources :recipes, only: [:index, :show]
   resources :public_recipes, only: [:index]
