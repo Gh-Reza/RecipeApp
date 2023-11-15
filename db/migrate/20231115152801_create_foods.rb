df39a589b7b2edcf3ee9fb7eda86cbf0e4e89c4b
Reza Merzaie <ghrezam97@gmail.com>
2023-11-15 20:53:56 +0430

    Some general modification

--- db/migrate/20231115152801_create_foods.rb
+++ db/migrate/20231115152801_create_foods.rb
@@ -2,8 +2,8 @@ class CreateFoods < ActiveRecord::Migration[7.1]
   def change
     create_table :foods do |t|
       t.string :name
-      t.dicimal :measurement_unit
-      t.dicimal :price
+      t.decimal :measurement_unit
+      t.decimal :price
       t.integer :quantity
       t.references :user, foreign_key: true
       t.timestamps
