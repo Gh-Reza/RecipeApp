a876b46d555286d6a438ab8ca6705aff96a686dd
Reza Merzaie <ghrezam97@gmail.com>
2023-11-16 01:00:12 +0430

    Fix linter errors

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,12 +1,11 @@
-require "test_helper"
+require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  test "confirmations" do
+  test 'confirmations' do
     mail = UserMailer.confirmations
-    assert_equal "Confirmations", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal 'Confirmations', mail.subject
+    assert_equal ['to@example.org'], mail.to
+    assert_equal ['from@example.com'], mail.from
+    assert_match 'Hi', mail.body.encoded
   end
-
 end
