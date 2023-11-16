cfb058aabfaa94e64270452f82982716ff29c642
Reza Merzaie <ghrezam97@gmail.com>
2023-11-16 13:37:59 +0430

    Add recipe addition functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,16 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new 
   end
 
+  def create
+    @recipe = Recipe.new(params.require(:recipe).permit(:name, :description, :prepraration_time, :cooking_time))
+    @recipe.user_id = current_user.id
+    if @recipe.save
+      redirect_to recipes_path, notice: 'Recipe was successfully created.'
+    else
+      render :new
+    end
+  end
+
   def destroy
     @recipe = Recipe.find(params[:id])
     @recipe.destroy
