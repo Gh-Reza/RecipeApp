da2b9920191b86cdccea79c3edb8058a8ef41bc9
Reza Merzaie <ghrezam97@gmail.com>
2023-11-17 10:45:13 +0430

    Display the selected recipe and its details

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,11 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+    @foods = @recipe.foods
+  end
+
   def create
     @recipe = Recipe.new(params.require(:recipe).permit(:name, :description, :prepraration_time, :cooking_time))
     @recipe.user_id = current_user.id
@@ -22,4 +27,18 @@ class RecipesController < ApplicationController
     @recipe.destroy
     redirect_to recipes_path, notice: 'Recipe was successfully deleted.'
   end
+
+  def update_public
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      render json: { status: 'success' }
+    else
+      render json: { status: 'error', errors: @recipe.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:public)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'users#index'
   devise_for :users
   resources :foods
-  resources :recipes
+  resources :recipes do
+    member do
+      patch :update_public
+    end
+  end
   resources :users
 end
