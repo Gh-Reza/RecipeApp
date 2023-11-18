1c75fd7f8289a6c60f64477ffe270b13dab96dbf
Marc raphael <marcraphael979@gmail.com>
2023-11-18 11:33:57 +0100

    Add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  include CanCan::ControllerAdditions
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username email password password_confirmation])
   end
 end
