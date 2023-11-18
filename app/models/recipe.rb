a419fb3bb114d7a6b27fe5623d49fdd8738c0587
Marc raphael <marcraphael979@gmail.com>
2023-11-18 13:19:43 +0100

    resolve conflict

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,15 @@
 class Recipe < ApplicationRecord
-  belongs_to :user
-  has_many :recipe_foods, dependent: :destroy
-  has_many :foods, through: :recipe_foods
+  validates :name, presence: true
+  validates :prepraration_time, presence: true
+  validates :cooking_time, presence: true
 
-  validates :name, presence: true, length: { maximum: 250 }
+  def total_foods_price
+    total_price = 0
+    foods.each do |food|
+      recipe_food = RecipeFood.find_by(recipe_id: id, food_id: food.id)
+      food_total_price = food.price * recipe_food.quantity
+      total_price += food_total_price
+    end
+    total_price
+  end
 end
