1a101526740061e47a0d7164868ac60ec0eff0c4
Marc raphael <marcraphael979@gmail.com>
2023-11-19 02:16:01 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_17_155214) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_14_215314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,8 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_17_155214) do
     t.string "name"
     t.string "measurement_unit"
     t.decimal "price"
-    t.integer "quantity"
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
@@ -27,8 +26,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_17_155214) do
 
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
-    t.bigint "recipe_id"
-    t.bigint "food_id"
+    t.bigint "recipe_id", null: false
+    t.bigint "food_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["food_id"], name: "index_recipe_foods_on_food_id"
@@ -37,30 +36,25 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_17_155214) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.integer "prepraration_time"
-    t.integer "cooking_time"
-    t.string "description"
-    t.boolean "public", default: false
-    t.bigint "user_id"
+    t.decimal "preparation_time"
+    t.decimal "cooking_time"
+    t.text "description"
+    t.boolean "public"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "#<ActiveRecord::ConnectionAdapters::PostgreSQL::TableDefinition"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
